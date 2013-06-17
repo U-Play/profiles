3ee5d1d140620f054a09c0c89e208bbd9671b447
Miguel Palhas <mpalhas@gmail.com>
2013-06-18 00:11:59 +0100

    initial commit. raw rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UPlay::Application.config.secret_token = 'fa4ba40fc7e87a9ec10cf30bc749b5784ef22a570fc706010be762bc536bf7aa22c70afd1a4d1129fa0bced4bfd08190d463fb60dea2989d912de88c320c11ba'
