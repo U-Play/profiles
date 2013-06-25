0ce092637c9949a41b4ce9c68204f132eb4e4461
Gabriel Poça <gabrielpoca@gmail.com>
2013-06-25 10:38:23 +0100

    Making basic information.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Profiles::Application.routes.draw do
+  get '/user/:id' => "users#view"
   root to: 'pages#home'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
