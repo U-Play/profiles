7b9f0790eef76b6a2643a1416841707835d9fd2d
Gabriel Poça <gabrielpoca@gmail.com>
2013-07-02 15:46:11 +0100

    Renamed athlete to user

--- features/step_definitions/view_profile_steps.rb
+++ features/step_definitions/view_profile_steps.rb
@@ -1,20 +1,20 @@
 Given(/^I am a user$/) do
 end
 
-Given(/^there is an athlete$/) do
-  @athlete = Presenters::User.new create(:user)
+Given(/^there is an user$/) do
+  @user = Presenters::User.new create(:user)
 end
 
-When(/^I go to the athlete's profile$/) do
-  visit profile_path(@athlete.user.id)
+When(/^I go to the user's profile$/) do
+  visit profile_path(@user.user.id)
 end
 
-Then(/^I should see his information$/) do
-  page.should have_content @athlete.full_name
+Then(/^I should see his information|I should see all my information$/) do
+  page.should have_content @user.full_name
 end
 
 Given(/^I am a signed user$/) do
-  @athlete = Presenters::User.new create(:user)
+  @user = Presenters::User.new create(:user)
   step "I go to the sign in page"
   step "I sign up via facebook"
 end
@@ -22,8 +22,3 @@ end
 When(/^I go to my profile$/) do
   visit my_profile_path
 end
-
-Then(/^I should see all my information$/) do
-  page.should have_content @athlete.full_name
-end
-
--- features/view_profile.feature
+++ features/view_profile.feature
@@ -2,13 +2,13 @@ Feature: Viewing a profile
   As a user
   I want to view a profile
 
-  Scenario: View an athlete profile
+  Scenario: View an user profile
     Given I am a visitor
-    And there is an athlete
-    When I go to the athlete's profile
+    And there is an user
+    When I go to the user's profile
     Then I should see his information
 
-  Scenario: View my athlete profile
+  Scenario: View my profile
     Given I am a signed user
     When I go to my profile
     Then I should see all my information
