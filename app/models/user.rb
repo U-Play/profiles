f14bd4da61a623991c07b9f3c93c3b81cf2e61bc
Miguel Palhas <mpalhas@gmail.com>
2013-07-09 11:01:34 +0100

    all green

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   attr_accessible :email,
                   :first_name,
                   :last_name,
+                  :password,
                   :profile_finished,
                   :remember_me
 
