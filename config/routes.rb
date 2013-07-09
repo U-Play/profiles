67573f6970d0d3e366c80941067b05dfe5e6059b
Miguel Palhas <mpalhas@gmail.com>
2013-07-09 10:48:52 +0100

    User is redirected to his edit page if his profile is not complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Profiles::Application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  get '/user/:id/edit' => 'users#edit', as: :user_edit
 end
