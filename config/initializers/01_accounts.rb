86de1b3d160ee29cd9fcaca77879fd6eb67315df
Gabriel Poça <gabrielpoca@gmail.com>
2013-07-16 11:14:25 +0100

    fixed accounts for facebook production app

--- config/initializers/01_accounts.rb
+++ config/initializers/01_accounts.rb
@@ -1,5 +1,5 @@
 file_name = "#{Rails.root}/config/accounts.yml"
 
 unless Rails.env.test? || !File.exists?(file_name) or defined?(ACCOUNTS)
-  ACCOUNTS = YAML.load_file(file_name)
+  ACCOUNTS = YAML.load_file(file_name)[Rails.env]
 end
