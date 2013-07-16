9861ede6b496470e71b5bd04cefefb849d445100
Miguel Palhas <mpalhas@gmail.com>
2013-07-16 10:19:45 +0100

    users first and last name no longer required to exist in DB
    This fixes an error when for some this data does not come from facebook

--- db/migrate/20130617232345_add_data_to_users.rb
+++ db/migrate/20130617232345_add_data_to_users.rb
@@ -1,8 +1,8 @@
 class AddDataToUsers < ActiveRecord::Migration
   def change
     change_table :users do |t|
-      t.string :first_name, null: false, default: ""
-      t.string :last_name,  null: false, default: ""
+      t.string :first_name
+      t.string :last_name
       t.date :birth_date
       t.text :bio
       t.string :facebook_link
