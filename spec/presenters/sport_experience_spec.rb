148eb918bc9b8f8f4a79c15c342a1add4f0ca67a
Gabriel Poça <gabrielpoca@gmail.com>
2013-07-20 15:56:21 +0100

    Implements the view achievement feature.

--- spec/presenters/sport_experience_spec.rb
+++ spec/presenters/sport_experience_spec.rb
@@ -19,4 +19,23 @@ describe SportExperiencePresenter do
     end
   end
 
+  context "#date" do
+    it "returns the correct string if there is no end date" do
+        sport_experience = build(:sport_experience, end_date: nil)
+        presenter = SportExperiencePresenter.new(sport_experience, view)
+
+        presenter.date.should eq t 'sport_experience.date_onwards',
+          date: sport_experience.start_date.year
+    end
+
+    it "returns the correct string if there is begin and end date" do
+        sport_experience = build(:sport_experience)
+        presenter = SportExperiencePresenter.new(sport_experience, view)
+
+        presenter.date.should eq t 'sport_experience.date',
+          start_date: sport_experience.start_date.year,
+          end_date: sport_experience.end_date.year
+    end
+  end
+
 end
