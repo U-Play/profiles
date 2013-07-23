097e0265bf6755b535533405e979dbe893cd5bc8
Gabriel Poça <gabrielpoca@gmail.com>
2013-07-23 15:30:50 +0100

    Adds mixpanel library and basic events.
    * Adds mixpanel library and token in accounts file.
    * Adds object to make call to mixpanel library.

---
+++ app/profiles/services/mixpanel_register.rb
@@ -0,0 +1,20 @@
+module Services
+  class MixpanelRegister
+    def initialize(user)
+      @user = user
+      @tracker = Mixpanel::Tracker.new(ACCOUNTS['mixpanel']['token'])
+    end
+
+    def register
+      tracker.people.set(user.id, {
+          '$first_name'       => user.first_name,
+          '$last_name'        => user.last_name,
+          '$email'            => user.email
+      });
+    end
+
+    private
+
+    attr_reader :user, :tracker
+  end
+end
