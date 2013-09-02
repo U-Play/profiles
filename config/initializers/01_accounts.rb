8a6cdcb7f70ad2ab6749d02a02ec49af871104d9
Gabriel Poça <gabrielpoca@gmail.com>
2013-09-02 10:52:50 +0100

    Adds deploy configuration for multi stage.

--- config/initializers/01_accounts.rb
+++ config/initializers/01_accounts.rb
@@ -1,5 +1,5 @@
 unless defined?(ACCOUNTS) || Rails.env.test?
-  if Rails.env.production?
+  if Rails.env.production? || Rails.env.staging?
     file_name = '/home/deploy/config/accounts.yml'
   else
     file_name = "#{Rails.root}/config/accounts.yml"
