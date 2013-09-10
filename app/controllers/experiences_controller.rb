e262aa9b1906fe8a048aa0716b51e6249ce535ac
Miguel Palhas <mpalhas@gmail.com>
2013-09-10 13:45:31 +0100

    sanitizes empty tournaments on submit

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,6 +2,8 @@ class ExperiencesController < ApplicationController
 
   layout 'profile'
 
+  before_filter :sanitize_empty_tournaments, only: [:create, :update]
+
   def create
     @experience = current_user.experiences.build params[:experience]
 
@@ -27,6 +29,14 @@ class ExperiencesController < ApplicationController
     else
       flash.now[:alert] = @experience.errors.full_messages.first
       render :edit
+    end
+  end
+
+  def sanitize_empty_tournaments
+    params[:experience][:tournaments_attributes].each do |key, tournament|
+      if tournament["name"].empty? && tournament["achievements"].empty? && tournament["award_date(1i)"].empty?
+        params[:experience][:tournaments_attributes].delete key
+      end
     end
   end
 end
