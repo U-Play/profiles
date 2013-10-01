051ac188f0b2dee43b9b426468de4c51375720ca
Miguel Palhas <mpalhas@gmail.com>
2013-10-01 17:47:41 +0100

    ditches wip page for now in production as well (do we really need it?)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(user)
-    user_redirect_path(user)
-  end
-
-  def user_redirect_path(user)
-    if Rails.env.production?
-      wip_path
-    elsif user.profile_finished?
+    if user.profile_finished?
       my_profile_path
     else
       edit_my_profile_path
