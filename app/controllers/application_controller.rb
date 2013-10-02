ea0fdeb8c336df1576952eeb6206cafe180f8c93
Bruno Azevedo <azevedo.252@gmail.com>
2013-10-02 13:37:05 +0100

    bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(user)
-    if user.profile_finished?
+    if user.profile_complete?
       my_profile_path
     else
       edit_my_profile_path
