68f97d4d87e9f9dce7d6270c5cf87abb1fe6eb73
Miguel Palhas <mpalhas@gmail.com>
2013-10-03 11:59:45 +0100

    team form fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
     params = flash[:new_experience_params]
     @experience_hide = params.nil?
     @new_experience = current_user.experiences.build(params)
+    @new_experience.tournaments.build
   end
 
 end
