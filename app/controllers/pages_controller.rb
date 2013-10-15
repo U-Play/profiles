bcf0216ab5534be805eda204a0d1c7dd346d7716
Bruno Azevedo <azevedo.252@gmail.com>
2013-10-15 17:54:58 +0100

    landing page done. not responsive

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,13 @@ class PagesController < ApplicationController
     redirect_to after_sign_in_path_for(current_user) if current_user
 
     @token = params[:token]
+
+    @users = []
+    if Rails.env == 'production'
+      @users << present(User.find(12))
+      @users << present(User.find(16))
+      @users << present(User.find(44))
+    end
   end
 
   def about
