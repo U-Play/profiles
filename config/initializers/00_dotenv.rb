07a70edcb16709febe058d27c559c5bcfc814c9c
Miguel Palhas <mpalhas@gmail.com>
2013-10-17 10:36:35 +0100

    adds dotenv

---
+++ config/initializers/00_dotenv.rb
@@ -0,0 +1,9 @@
+Profiles::Application.configure do
+
+  # action_mailer
+  if ENV['PROFILES_ACTION_MAILER_URL']
+    mailer_options = { host: ENV['PROFILES_ACTION_MAILER_URL'] }
+    mailer_options[:port] = ENV['PROFILES_ACTION_MAILER_PORT'] if ENV['PROFILES_ACTION_MAILER_PORT']
+    config.action_mailer.default_url_options = mailer_options
+  end
+end
