a2489ca70ac435bed8b5d73413c8a0fde64a7ad3
Miguel Palhas <mpalhas@gmail.com>
2013-10-18 17:25:45 +0100

    fixes email

---
+++ config/initializers/setup_mailer.rb
@@ -0,0 +1,19 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+
+unless Rails.env.production? || Rails.env.staging?
+  ActionMailer::Base.default_url_options[:port] = 3000
+end
+
+if ENV['PROFILES_ACTION_MAILER_USERNAME']
+  ActionMailer::Base.default_url_options[:host] = ENV['PROFILES_ACTION_MAILER_HOST']
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['PROFILES_ACTION_MAILER_USERNAME'],
+    password:  ENV['PROFILES_ACTION_MAILER_PASSWORD'],
+    domain:    ENV['PROFILES_ACTION_MAILER_DOMAIN'],
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
+end
